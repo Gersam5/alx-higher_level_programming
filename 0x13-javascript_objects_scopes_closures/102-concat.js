1791a680b99cacb1dc443b246d03bc4c98d968fd
Gersam5 <gersamsemu5@gmail.com>
2024-03-12 17:35:21 -0700

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,6 @@
-#!/usr/bin/node
-const file = require('fs');
-const fileA = file.readFileSync(process.argv[2], 'utf8');
-const fileB = file.readFileSync(process.argv[3], 'utf8');
-file.writeFileSync(process.argv[4], fileA + fileB);
+!/usr/bin/node
+const fs = require('fs');
+
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
